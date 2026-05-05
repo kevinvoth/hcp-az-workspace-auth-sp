7b000707b27355587001e33025a737e75f13a0ee
Kevin <kevin@smithvoth.ca>
2026-05-04 22:14:23 -0400

    Added azurerm provider details

--- provider.tf
+++ provider.tf
@@ -1,4 +1,13 @@
 terraform {
+  required_version = ">= 1.15.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.71.0"
+    }
+  }
+
   cloud {
     organization = "vothdev"
 
@@ -13,5 +22,3 @@ provider "azurerm" {
 
   subscription_id = "1b7ba0c4-0c98-4572-947e-0759a7cd2216"
 }
-
-
