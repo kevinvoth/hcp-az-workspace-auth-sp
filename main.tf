1cb33ccd895d60f97713a606450938549f371670
Kevin <kevin@smithvoth.ca>
2026-05-06 21:26:32 -0400

    Replace group code with a group module

---
+++ main.tf
@@ -0,0 +1,16 @@
+
+module "rg_create" {
+  source  = "app.terraform.io/vothdev/azure-infra-rg/azurerm"
+  version = "1.1.0"
+
+  name     = "hcp-az-cac-workspace-auth-sp"
+  location = "canadacentral"
+}
+
+module "rg_create" {
+  source  = "app.terraform.io/vothdev/azure-infra-rg/azurerm"
+  version = "1.1.0"
+
+  name     = "hcp-az-cac-workspace-auth-sp"
+  location = "canadacentral"
+}
